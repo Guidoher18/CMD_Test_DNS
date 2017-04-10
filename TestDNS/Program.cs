406baea56f2b1a2568d6b25f6d5cc7fd1cb22028
Guidoher18 <guido_181193@outlook.com>
2017-04-10 00:25:23 -0300

    Comienzo de la App

---
+++ TestDNS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahora Vamos a Testear la velocidad de conexion con los DNS");
+            Console.WriteLine("TELEFÓNICA DNS----------------------");
+            Console.WriteLine("ping 200.51.211.7");
+            Console.WriteLine("ping 200.51.212.7");
+
+            Console.WriteLine("OPEN DNS ----------------------");
+            Console.WriteLine("ping 208.67.222.222");
+            Console.WriteLine("ping 208.67.220.220");
+
+            Console.WriteLine("GOOGLE DNS ----------------------");
+            Console.WriteLine("ping 8.8.8.8");
+            Console.WriteLine("ping 4.4.4.4");
+            Console.WriteLine("Presione una Tecla para Salir");
+            
+            Console.ReadKey();
+        }
+    }
+}
