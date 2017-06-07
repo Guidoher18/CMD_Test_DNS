9c067b7b34a0931a02cd8c70f3992dbe66400383
Guidoher18 <guido_181193@outlook.com>
2017-06-07 01:32:00 -0300

    Comenzando el form...

--- TestDNS/Program.cs
+++ TestDNS/Program.cs
@@ -18,73 +18,37 @@ namespace TestDNS
             Ping("208.67.220.220", "Open DNS2");
             Ping("8.8.8.8", "Google DNS1");
             Ping("8.8.4.4", "Google DNS2");
+            Console.WriteLine("Presione una tecla para salir...");
             Console.ReadKey();
         }
+
         static void Ping(string Dns, string Nombre)
         {
-            // Abro una nueva instancia de Process para comenzar un proceso
+            // Inicio una nueva instancia de Process para comenzar un proceso
             Process consola = new Process();
-            // Establesco que aplicación voy a abrir y con qué argumentos. También establezco que la ventana este oculta
+            
+            // Establezco que aplicación voy a abrir y con qué argumentos.
             consola.StartInfo.FileName = "cmd.exe";
             consola.StartInfo.Arguments = "/C ping " + Dns;
-            //consola.WindowStyle = ProcessWindowStyle.Hidden;
             consola.StartInfo.UseShellExecute = false;
             consola.StartInfo.ErrorDialog = false;
+            
+            //El proceso sucede oculto para el usuario
             consola.StartInfo.CreateNoWindow = true;
+            
+            // Tomo el output y lo redirijo 
             consola.StartInfo.RedirectStandardOutput = true;
             consola.StartInfo.RedirectStandardError = true;
+            
             // Comienzo el Proceso
             consola.Start();
 
+            //Escribo en la consola el resultado
             string salida = consola.StandardOutput.ReadToEnd();
             Console.WriteLine(Nombre);
             Console.WriteLine(salida);
-            //Console.ReadKey();
             consola.Close();
 
-
-
-
-            /*Process.Start("cmd.exe", );
-            Console.WriteLine("Ahora Vamos a Testear la velocidad de conexion con los DNS");
-            Console.WriteLine("TELEFÓNICA DNS----------------------");
-            Console.WriteLine("ping 200.51.211.7");
-            Console.WriteLine("ping 200.51.212.7");
-
-            Console.WriteLine("OPEN DNS ----------------------");
-            Console.WriteLine("ping 208.67.222.222");
-            Console.WriteLine("ping 208.67.220.220");
-
-            Console.WriteLine("GOOGLE DNS ----------------------");
-            Console.WriteLine("ping 8.8.8.8");
-            Console.WriteLine("ping 4.4.4.4");
-            Console.WriteLine("Presione una Tecla para Salir");
-
-            Console.ReadKey(); 
-         }
-
-         /*static void ExecuteCommand()
-         {
-             //Indicamos que deseamos inicializar el proceso cmd.exe junto a un comando de arranque. 
-             //(/C, le indicamos al proceso cmd que deseamos que cuando termine la tarea asignada se cierre el proceso).
-             //Para mas informacion consulte la ayuda de la consola con cmd.exe /? 
-             ProcessStartInfo consola = new ProcessStartInfo("cmd.exe", "ping 200.51.211.7");
-             // Indicamos que la salida del proceso se redireccione en un Stream
-             consola.RedirectStandardOutput = true;
-             consola.UseShellExecute = true;
-             //Indica que el proceso no despliegue una pantalla negra (El proceso se ejecuta en background)
-             consola.CreateNoWindow = false;
-             //Inicializa el proceso
-             Process proc = new Process();
-             proc.StartInfo = consola;
-             proc.Start();
-             //Consigue la salida de la Consola(Stream) y devuelve una cadena de texto
-             string result = proc.StandardOutput.ReadToEnd();
-             //Muestra en pantalla la salida del Comando
-             Console.WriteLine(result);
-             Console.ReadKey();
-         }
-         */
         }
 
         
